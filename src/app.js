3aa69637d3072a3b8699b802f9646b49d500faac
akshat-khulbe-envi <akshat@envistride.com>
2026-08-18 09:42:41 +0530

    created the signup api for the dev tiunder

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/user");
 
 const { adminAuth } = require("./middleware/auth");
 const app = express();
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  const data = req.body;
+  console.log("data", data);
+  try {
+    const user = new User(data);
+    await user.save();
+    res.send("User created successfully");
+  } catch (error) {
+    res.status(400).json({
+      message: "Error Creating the User",
+      error: error.message,
+    });
+  }
+});
 
 connectDB()
   .then(() => {
